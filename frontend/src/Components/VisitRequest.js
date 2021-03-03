4a64e598ade9f79666b2ca1bb71bc0059a7f27e9
AgroLucas <lucasagro1122@gmail.com>
2021-03-03 17:16:58 +0100

    refactor - change name of RequestVisit into VisitRequest

---
+++ frontend/src/Components/VisitRequest.js
@@ -0,0 +1,7 @@
+let page = document.querySelector("#page");
+
+const RequestVisit = () => {
+    page.innerHTML = `<h1>Page pour demander une visite du site (clients)</h1>`;
+}
+
+export default RequestVisit;
