6ece6013a3a43092c263faf15c4854fa49aa2bef
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-03-05 15:54:05 +0100

    feat - implement server start in main

---
+++ src/main/java/be/vinci/pae/main/ApplicationBinder.java
@@ -0,0 +1,14 @@
+package be.vinci.pae.main;
+
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.ext.Provider;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+
+@Provider
+public class ApplicationBinder extends AbstractBinder {
+
+  @Override
+  protected void configure() {
+    //bind(implClassname.class).to(factoryname.class).in(Singleton.class);
+  }
+}
--- src/main/java/be/vinci/pae/main/Main.java
+++ src/main/java/be/vinci/pae/main/Main.java
@@ -1,19 +1,41 @@
 package be.vinci.pae.main;
 
 
+import java.io.IOException;
+import java.net.URI;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
 /**
  * Main class.
  */
 public class Main {
+
+  public static HttpServer startServer() {
+    final ResourceConfig rc = new ResourceConfig().packages("be.vinci.pae.presentation")
+        .register(JacksonFeature.class)
+        .register(ApplicationBinder.class).property("jersey.config.server.wadl.disableWadl", true);
+
+    return GrizzlyHttpServerFactory.createHttpServer(URI.create(Configurate.getConfiguration("BaseUri")), rc);
+  }
 
   /**
    * Main method.
    *
    * @param args command line arguments
    */
-  public static void main(String[] args) {
-
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws IOException {
+    //load properties file
+    Configurate.load("src.main.resources.prod.properties");
+    //start server
+    final HttpServer server = startServer();
+    System.out.println("Jersey app started at " + Configurate.getConfiguration("BaseUri"));
+    //Listen to key press and shut down
+    System.out.println("Hit a key to stop it...");
+    System.in.read();
+    server.shutdown();
 
   }
 
