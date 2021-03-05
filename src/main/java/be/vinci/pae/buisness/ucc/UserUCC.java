46864ffec959150ab259ce1499b6baff3b51577a
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-03-05 12:00:44 +0100

    feat - add javadoc for methods in UserUCC and UserDAO interfaces

--- src/main/java/be/vinci/pae/buisness/ucc/UserUCC.java
+++ src/main/java/be/vinci/pae/buisness/ucc/UserUCC.java
@@ -3,5 +3,12 @@ package be.vinci.pae.buisness.ucc;
 import be.vinci.pae.buisness.dto.UserDTO;
 
 public interface UserUCC {
+
+    /**
+     * Logs in after checking the given credentials.
+     * @param username : the given username
+     * @param password : the given password
+     * @return the corresponding UserUCC or null if invalid credentials
+     */
     UserDTO login(String username, String password);
 }
--- src/main/java/be/vinci/pae/persistence/dao/UserDAO.java
+++ src/main/java/be/vinci/pae/persistence/dao/UserDAO.java
@@ -3,5 +3,11 @@ package be.vinci.pae.persistence.dao;
 import be.vinci.pae.buisness.dto.UserDTO;
 
 public interface UserDAO {
+
+    /**
+     * Executes a query to find a user having a specific username.
+     * @param username : the username to look for
+     * @return a UserDTO corresponding to the user found or null if no user has the given username.
+     */
     UserDTO findByUsername(String username);
 }
