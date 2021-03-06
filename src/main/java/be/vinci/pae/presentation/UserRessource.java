a80fecc48863eba490f74e836e897cedea499f16
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-03-06 15:22:18 +0100

    feat - finish UserResource.login

--- src/main/java/be/vinci/pae/presentation/UserRessource.java
+++ src/main/java/be/vinci/pae/presentation/UserRessource.java
@@ -9,7 +9,10 @@ import jakarta.inject.Singleton;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 
 @Singleton
 @Path("/users")
@@ -25,9 +28,17 @@ public class UserRessource {
   @Path("login")
   @Produces(MediaType.APPLICATION_JSON)
   public UserDTO login(User user) {
-    if (user.getUsername() == null || user.getPassword() == null)
-      return null;
+    if (user.getUsername() == null || user.getPassword() == null) { // invalid request
+      throw new WebApplicationException(
+          Response.status(Status.BAD_REQUEST).entity("Lacks mandatory info").type("text/plain")
+              .build());
+    }
     UserDTO res = userUCC.login(user.getUsername(), user.getPassword());
+    if (res == null) { // user not found
+      throw new WebApplicationException(
+          Response.status(Status.NOT_FOUND).entity("Resource not found").type("text/plain")
+              .build());
+    }
     return res;
   }
 
