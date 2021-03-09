4813c7db350e15d2f43e7a6e8ec57a4778edbe8a
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-03-09 21:40:07 +0100

    feat - Views class + Json service class (static filter method)

---
+++ src/main/java/be/vinci/pae/main/Views.java
@@ -0,0 +1,9 @@
+package be.vinci.pae.main;
+
+public class Views {
+  public static class Public {
+  }
+
+  public static class Internal extends Public {
+  }
+}
---
+++ src/main/java/be/vinci/pae/utils/Json.java
@@ -0,0 +1,22 @@
+package be.vinci.pae.utils;
+
+import be.vinci.pae.main.Views;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Json {
+
+  private final static ObjectMapper jsonMapper = new ObjectMapper();
+
+  public static <T> T filterPublicJsonView(T item, Class<T> targetClass) {
+    try {
+      String publicItemAsString = jsonMapper.writerWithView(Views.Public.class)
+          .writeValueAsString(item);
+      return jsonMapper.readerWithView(Views.Public.class).forType(targetClass)
+          .readValue(publicItemAsString);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+}
