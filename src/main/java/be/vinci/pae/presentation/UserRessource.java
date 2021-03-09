889742ce2b8f74a6ce30b457d287fcb3cb211ea5
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-03-09 10:31:48 +0100

    feat - /me in UserResource

--- src/main/java/be/vinci/pae/presentation/UserRessource.java
+++ src/main/java/be/vinci/pae/presentation/UserRessource.java
@@ -7,13 +7,16 @@ import be.vinci.pae.business.ucc.UserUCC;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
+import org.glassfish.jersey.server.ContainerRequest;
 
 @Singleton
 @Path("/users")
@@ -51,4 +54,12 @@ public class UserRessource {
     return Response.ok(userDTO, MediaType.APPLICATION_JSON).build();
   }
 
+  @GET
+  @Path("me")
+  @Produces(MediaType.APPLICATION_JSON)
+  //@Authorize
+  public Response getUser(@Context ContainerRequest request) {
+    UserDTO currentUser = (UserDTO) request.getProperty("user");
+    return Response.ok(currentUser, MediaType.APPLICATION_JSON).build();
+  }
 }
