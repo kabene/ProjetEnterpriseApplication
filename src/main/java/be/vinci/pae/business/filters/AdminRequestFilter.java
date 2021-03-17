9a95392cfdfceec614e4dca4c9e67b514922e668
Lucas Agro <lucas.y.agro@student.vinci.be>
2021-03-17 19:47:50 +0100

    fix - remove unused imports

--- src/main/java/be/vinci/pae/business/filters/AdminRequestFilter.java
+++ src/main/java/be/vinci/pae/business/filters/AdminRequestFilter.java
@@ -1,15 +1,8 @@
 package be.vinci.pae.business.filters;
 
-import be.vinci.pae.business.dto.UserDTO;
-import be.vinci.pae.utils.Configurate;
 import be.vinci.pae.persistence.dao.UserDAO;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.JWTVerifier;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.core.Response;
