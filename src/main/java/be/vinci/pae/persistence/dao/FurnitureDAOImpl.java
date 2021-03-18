14b1935133c4b65bb551f1704996d9ceeda8b884
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-03-18 08:55:50 +0100

    feat - create FurnitureDTO

---
+++ src/main/java/be/vinci/pae/persistence/dao/FurnitureDAOImpl.java
@@ -0,0 +1,24 @@
+package be.vinci.pae.persistence.dao;
+
+import be.vinci.pae.business.dto.FurnitureDTO;
+import be.vinci.pae.business.factories.FurnitureFactory;
+import jakarta.inject.Inject;
+
+public class FurnitureDAOImpl implements FurnitureDAO{
+
+  @Inject
+  private static FurnitureFactory furnitureFactory;
+
+  /**
+   * Finds one piece of furniture in the db having a specific id.
+   *
+   * @param id : the id of the piece of furniture
+   * @return The piece of furniture as a FurnitureDTO
+   */
+  @Override
+  public FurnitureDTO findById(int id) {
+    FurnitureDTO res = furnitureFactory.getFurnitureDTO(); //stub
+    res.setFurnitureId(id);
+    return res;
+  }
+}
