df48018495d32dfbe388bf78c200e3413dc4bab7
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-03-22 23:21:49 +0100

    feat - implement dao

--- src/main/java/be/vinci/pae/persistence/dao/FurnitureTypeDAOImpl.java
+++ src/main/java/be/vinci/pae/persistence/dao/FurnitureTypeDAOImpl.java
@@ -1,9 +1,32 @@
 package be.vinci.pae.persistence.dao;
 
+import be.vinci.pae.persistence.dal.ConnectionBackendDalServices;
+import jakarta.inject.Inject;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class FurnitureTypeDAOImpl implements FurnitureTypeDAO{
+
+  @Inject
+  ConnectionBackendDalServices dalServices;
 
   @Override
   public String findById(int id) {
-    return null;
+    String res = "";
+    String query = "SELECT t.* FROM satchofurniture.furniture_types t WHERE t.type_id = ?";
+    try {
+      PreparedStatement ps = dalServices.makeStatement(query);
+      ps.setInt(1, id);
+      ResultSet rs = ps.executeQuery();
+      if(rs.next()) {
+        res = rs.getString("type_name");
+      }
+      rs.close();
+      ps.close();
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    }
+    return res;
   }
 }
