b9601b42284031af73e0b1975d14cac11b599c17
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-03-23 15:41:04 +0100

    feat - implement FurnitureDAO method getAll

--- src/main/java/be/vinci/pae/persistence/dao/FurnitureDAOImpl.java
+++ src/main/java/be/vinci/pae/persistence/dao/FurnitureDAOImpl.java
@@ -26,7 +26,7 @@ public class FurnitureDAOImpl implements FurnitureDAO {
    */
   @Override
   public FurnitureDTO findById(int id) {
-    FurnitureDTO res = furnitureFactory.getFurnitureDTO();
+    FurnitureDTO res = null;
     String query = "SELECT f.* FROM satchofurniture.furniture f WHERE f.furniture_id = ?";
 
     try {
@@ -34,28 +34,7 @@ public class FurnitureDAOImpl implements FurnitureDAO {
       ps.setInt(1, id);
       ResultSet rs = ps.executeQuery();
       if (rs.next()) {
-        res.setFurnitureId(rs.getInt("furniture_id"));
-        res.setBuyerId(rs.getInt("buyer_id"));
-        res.setSellerId(rs.getInt("seller_id"));
-        res.setCondition(rs.getString("condition"));
-        Date saleWithdrawalDate = rs.getDate("sale_withdrawal_date");
-        if (saleWithdrawalDate != null) {
-          res.setSaleWithdrawalDate(saleWithdrawalDate.toString());
-        }
-        res.setDescription(rs.getString("description"));
-        res.setTypeId(rs.getInt("type_id"));
-        res.setFavouritePhotoId(rs.getInt("favourite_photo_id"));
-        res.setSellingPrice(rs.getDouble("selling_price"));
-        res.setSpecialSalePrice(rs.getDouble("special_sale_price"));
-        Date dateOfSale = rs.getDate("date_of_sale");
-        if (dateOfSale != null) {
-          res.setDateOfSale(dateOfSale.toString());
-        }
-        res.setToPickUp(rs.getBoolean("is_to_pick_up"));
-        Date pickUpDate = rs.getDate("pick_up_date");
-        if (pickUpDate != null) {
-          res.setPickUpDate(pickUpDate.toString());
-        }
+        res = toDTO(rs);
       }
       rs.close();
       ps.close();
@@ -68,6 +47,58 @@ public class FurnitureDAOImpl implements FurnitureDAO {
 
   @Override
   public List<FurnitureDTO> findAll() {
-    return new ArrayList<>();
+    List<FurnitureDTO> res = new ArrayList<>();
+    String query = "SELECT f.* FROM satchofurniture.furniture f";
+    try {
+      PreparedStatement ps = dalServices.makeStatement(query);
+      ResultSet rs = ps.executeQuery();
+      while(rs.next()) {
+        res.add(toDTO(rs));
+      }
+      rs.close();
+      ps.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return res;
+  }
+
+  /**
+   * transfers ONE furniture entry from a result set to a dto.
+   *
+   * @param rs the result set containing the information.
+   * @return a dto containing the information from the result set
+   * @throws SQLException if an error occurs while reading the result set
+   */
+  private FurnitureDTO toDTO(ResultSet rs) throws SQLException {
+    FurnitureDTO res = furnitureFactory.getFurnitureDTO();
+    res.setFurnitureId(rs.getInt("furniture_id"));
+    res.setBuyerId(rs.getInt("buyer_id"));
+    res.setSellerId(rs.getInt("seller_id"));
+    res.setCondition(rs.getString("condition"));
+
+    Date saleWithdrawalDate = rs.getDate("sale_withdrawal_date");
+    if (saleWithdrawalDate != null) {
+      res.setSaleWithdrawalDate(saleWithdrawalDate.toString());
+    }
+
+    res.setDescription(rs.getString("description"));
+    res.setTypeId(rs.getInt("type_id"));
+    res.setFavouritePhotoId(rs.getInt("favourite_photo_id"));
+    res.setSellingPrice(rs.getDouble("selling_price"));
+    res.setSpecialSalePrice(rs.getDouble("special_sale_price"));
+
+    Date dateOfSale = rs.getDate("date_of_sale");
+    if (dateOfSale != null) {
+      res.setDateOfSale(dateOfSale.toString());
+    }
+
+    res.setToPickUp(rs.getBoolean("is_to_pick_up"));
+    Date pickUpDate = rs.getDate("pick_up_date");
+
+    if (pickUpDate != null) {
+      res.setPickUpDate(pickUpDate.toString());
+    }
+    return res;
   }
 }
