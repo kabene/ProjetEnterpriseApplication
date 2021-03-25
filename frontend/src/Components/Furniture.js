15be1daf5a123a8fbacf5ad96c0c71e45433c742
Lucas Agro <lucas.y.agro@student.vinci.be>
2021-03-25 20:15:44 +0100

    refactor - clean the code in Furniture.js

--- frontend/src/Components/Furniture.js
+++ frontend/src/Components/Furniture.js
@@ -8,41 +8,22 @@ import imageStub from "../img/furnitures/Bureau_1.png"
 let page = document.querySelector("#page");
 let furnitureList;
 let currentUser;
-let pageHTML;
 
 
 const Furniture = async () => {
     currentUser = getUserSessionData();
 
-    pageHTML = `<div class="text-center"><h2>Loading <div class="spinner-border"></div></h2></div>`;
-    page.innerHTML = pageHTML;
+    page.innerHTML = generateLoadingAnimation();
 
-    await fetch("/furniture/", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }).then((response) => {
-        if (!response.ok)
-            throw new Error(
-                "Error code : " + response.status + " : " + response.statusText
-            );
-        return response.json();
-    }).then((data) => {
-        furnitureList = data;
-    }).catch((err) => {
-        console.error(err);
-    });
-    pageHTML = generateTable();
-    page.innerHTML = pageHTML;
+    furnitureList = await getFurnitureList();
+
+    page.innerHTML = generateTable();
 }
 
 
 const generateTable = () => {
     let res = `<div class="row mx-5 p-5 border">`;
-    furnitureList.forEach(furniture => {
-        res += generateItem(furniture);
-    })
+    furnitureList.forEach(furniture => res += generateItem(furniture));
     res += `</div>`;
     return res;
 }
@@ -54,8 +35,36 @@ const generateItem = (furniture) => {
             <p> ` + furniture.description + ` </p>`;
     if (furniture.condition === "available for sale")
         res += `<button type="button" class="btn btn-sm btn-primary">Introduire une option</button>`;
-    //TODO add 'annuler option button + events when clicking on 'Introduire une option' button
+    //TODO add 'annuler option button + events when clicking on buttons
     res += `</div>`;
     return res;
 }
+
+
+const generateLoadingAnimation = () => {
+    return `
+        <div class="text-center">
+            <h2>Loading <div class="spinner-border"></div></h2>
+        </div>`
+}
+
+const getFurnitureList = async () => {
+    let ret = [];
+    await fetch("/furniture/", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((response) => {
+        if (!response.ok)
+            throw new Error("Error code : " + response.status + " : " + response.statusText);
+        return response.json();
+    }).then((data) => {
+         ret = data;
+    }).catch((err) => {
+        console.error(err);
+    });
+    return ret;
+}
+
 export default Furniture;
