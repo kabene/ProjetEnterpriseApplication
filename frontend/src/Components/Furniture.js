c47638accf23ebe545d3aa27de14be719f5024dc
Lucas Agro <lucas.y.agro@student.vinci.be>
2021-03-25 21:20:12 +0100

    add - modal structure

--- frontend/src/Components/Furniture.js
+++ frontend/src/Components/Furniture.js
@@ -45,7 +45,34 @@ const generateItemAndModal = (furniture) => {
                     <div class="modal-header"></div>
                     <!-- Modal Body -->
                     <div class="modal-body">
-                    
+                        <div class="row mx-0 pt-5">
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators bg-secondary">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img class="w-75 m-auto" src="` + imageStub + `" alt="Meuble 1">
+                                    </div>
+                                    <div class="carousel-item">
+                                            <img class="w-75 m-auto" src="` + imageStub + `" alt="Meuble 2">
+                                    </div>
+                                    <div class="carousel-item justify-content-center">
+                                        <img class="w-75 m-auto" src="` + imageStub + `" alt="Meuble 3">
+                                    </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>                         
