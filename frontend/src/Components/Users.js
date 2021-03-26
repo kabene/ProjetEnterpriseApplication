9ecc836281abd0191b5c927ad80e1b001f262974
Lucas Agro <lucas.y.agro@student.vinci.be>
2021-03-26 17:36:22 +0100

    feat - implement generateUserCard

--- frontend/src/Components/Users.js
+++ frontend/src/Components/Users.js
@@ -13,47 +13,48 @@ let pageHTML;
 const Users = async () => {
   currentUser = getUserSessionData();
 
-  pageHTML = `<div class="text-center"><h2>Loading <div class="spinner-border"></div></h2></div>`;
-  page.innerHTML = pageHTML;
+  page.innerHTML = generateLoadingAnimation();
 
-  await fetch("/users/detail", {
-    method: "GET",
-    headers: {
-      "Authorization": currentUser.token,
-      "Content-Type": "application/json",
-    },
-  }).then((response) => {
-    if (!response.ok) {
-      throw new Error(
-          "Error code : " + response.status + " : " + response.statusText
-      );
-    }
-    return response.json();
-  }).then((data) => {
-    usersList = data;
-  }).catch((err) => {
-    console.error(err);
-  });
+  usersList = await getUserList();
+  
+  page.innerHTML = generateUsersPage();
 
-  pageHTML = `
-    <h1>Liste des utilisateurs:</h1>
-    <div class="mx-5 row">
-        <div class="col-12">
-        <input type="text" placeholder="Rechercher par nom, prénom, code postal ou ville" class="w-50 mb-2">`
-      + generateLargeTable() +
-      `</div>
-        <div id="shortTable" class="col-4 collapse collapsedDiv">
-            <input type="text" placeholder="Rechercher" class="mb-2">
-            <button type="button" class="btn btn-dark mb-2" data-toggle="collapse" data-target=".collapsedDiv">Retour à la liste</button>`
-      + generateShortTable() +
-      `</div>
-        <div class="col-8 collapse collapsedDiv">`
-      + generateUserCard() +
-      `</div>
-    </div>`;
-  page.innerHTML = pageHTML;
-  //
+  document.querySelectorAll(".toBeClicked").forEach(element => element.addEventListener("click", addUserCard));
+
   await AddressToGeo("Clos Chapelle-aux-Champs 43, 1200 Woluwe-Saint-Lambert");
+}
+
+const addUserCard = async (e) => {
+  let element;
+  for (let i = 0; i < e.path.length; i++) {
+    if ((e.path[i].className + "").includes("toBeClicked")) {
+      element = e.path[i].attributes
+      break;
+    }
+  }
+  let id = element["userId"].value
+  let userDetail = await clientDetail(id);
+
+  let userCardDiv = document.getElementById("userCardDiv");
+  userCardDiv.innerHTML = generateUserCard(userDetail);
+}
+
+const generateUsersPage = () => {
+  return `
+        <h1>Liste des utilisateurs:</h1>
+        <div class="mx-5 row">
+            <div class="col-12">
+            <input type="text" placeholder="Rechercher par nom, prénom, code postal ou ville" class="w-50 mb-2">`
+          + generateLargeTable() +
+          `</div>
+            <div id="shortTable" class="col-4 collapse collapsedDiv">
+                <input type="text" placeholder="Rechercher" class="mb-2">
+                <button type="button" class="btn btn-dark mb-2" data-toggle="collapse" data-target=".collapsedDiv">Retour à la liste</button>`
+          + generateShortTable() +
+          `</div>
+            <div class="col-8 collapse collapsedDiv" id="userCardDiv">
+          </div>
+        </div>`;
 }
 
 const generateLargeTable = () => {
@@ -70,14 +71,32 @@ const generateLargeTable = () => {
                 <th>Rôle</th>
             </tr>
         </thead>
-        <tbody>`;
-  usersList.users.forEach(user => {
-    res += generateLargeRow(user);
-  });
-  res = res + `
+        <tbody>`
+          + getAllUsersLargeRows() + `
         </tbody>
     </table>`;
   return res;
+}
+
+const getAllUsersLargeRows = () => {
+  let res = "";
+  usersList.users.forEach(user => res += generateLargeRow(user));
+  return res;
+}
+
+
+const generateLargeRow = (user) => {
+  console.log(user);
+  return ` 
+    <tr class="toBeClicked" userId="` + user.id + `" data-toggle="collapse" data-target=".collapsedDiv">
+        <th><p>` + user.lastName + `</p></th>
+        <th><p>` + user.firstName + `</p></th>
+        <th><p>` + user.username + `</p></th>
+        <th><p>` + user.email + `</p></th>
+        <th><p>0 (STUB)</p></th>
+        <th><p>0 (STUB)</p></th>
+        <th><p>` + user.role + `</p></th>
+   </tr>`;
 }
 
 const generateShortTable = () => {
@@ -89,146 +108,147 @@ const generateShortTable = () => {
                 <th>Prénom</th>
             </tr>
         </thead>
-        <tbody>`;
-  usersList.users.forEach(user => {
-    res += generateShortRow(user);
-  });
-  res = res + `
-        </tbody>
+        <tbody>`
+          + generateAllUsersShortRows() +
+        `</tbody>
     </table>`;
   return res;
 }
 
-const generateLargeRow = (user) => {
-  return ` 
-    <tr class="toBeClicked" data-toggle="collapse" data-target=".collapsedDiv">
-        <th><p>` + user.lastName + `</p></th>
-        <th><p>` + user.firstName + `</p></th>
-        <th><p>` + user.username + `</p></th>
-        <th><p>` + user.email + `</p></th>
-        <th><p>0 (STUB)</p></th>
-        <th><p>0 (STUB)</p></th>
-        <th><p>` + user.role + `</p></th>
-   </tr>`;
+const generateAllUsersShortRows = () => {
+  let res = "";
+  usersList.users.forEach(user => res += generateShortRow(user));
+  return res;
 }
 
 const generateShortRow = (user) => {
   return ` 
-    <tr class="toBeClicked">
+    <tr class="toBeClicked" userId="` + user.id + `">
         <th><p>` + user.lastName + `</p></th>
         <th><p>` + user.firstName + `</p></th>
    </tr>`;
 }
 
 const generateUserCard = (userDetail) => {
+  console.log(userDetail);
+  console.log(userDetail.address);
   return `
    <div class="container emp-profile">
-            <form>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="profile-head">
-                                    <h5 id="Name&Firstname">
-                                      Doe John
-                                    </h5>
-                                    <p class="proile-rating" >ROLE : <span id="role"><p>     </p> </span></p>
-                            <ul class="nav nav-tabs" id="myTab" role="tablist">
-                                <li class="nav-item">
-                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">informations personneles</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">adresses et plan</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <input type="submit" class="profile-edit-btn" name="btnAddMore" value="prendre le controle"/>
-                    </div>
-                </div>
-                <div class="row">
-            
-                    <div class="col-md-8">
-                        <div class="tab-content profile-tab" id="myTabContent">
-                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>username</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p id="username"> </p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Nom et prénom:</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p id="Name&Firstname" >Doe John</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Email</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p id="email" >   </p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>nombre d'achat</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p id="purchased_furniture_nbr">   </p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>nombre de ventes</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p id="sold_furniture_nbr">   </p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>status</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p id="waiting">   </p>
-                                            </div>
-                                        </div>
-                                         <div class="col-md-2" style="display: flex"> 
-                        <input type="submit" class="profile-edit-btn" name="btnAddMore"  id="approuver" value="approuver" style="color: #0062cc; margin:5px" />
-                        <input type="submit" class="profile-edit-btn" name="btnAddMore" id="refuser" value="refuser" style="color: red; margin:5px"/>
-                    </div>
-                            </div>
-                    
-                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label> Adresse </label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p> Clos Chapelle-aux-Champs 43, 1200 Woluwe-Saint-Lambert</p>
-                                            </div>
-                                        </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <label>Map</label><br/>
-                                        <div id="map" ></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>           
+    <form>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="profile-head">
+            <h5 id="Name&Firstname">` + userDetail.lastName + ` ` + userDetail.firstName +`</h5>
+            <p class="proile-rating">ROLE : 
+              <span id="role">
+                <p>` + userDetail.role + `</p> 
+              </span>
+            </p>
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+              <li class="nav-item">
+                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">informations personnelles</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">adresses et plan</a>
+              </li>
+            </ul>
+          </div>
         </div>
+        <div class="col-md-2">
+          <input type="submit" class="profile-edit-btn" name="btnAddMore" value="prendre le controle"/>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-8">
+          <div class="tab-content profile-tab" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+
+              <div class="row">
+                <div class="col-md-6">
+                  <label>username</label>
+                </div>
+                <div class="col-md-6">
+                  <p id="username">` + userDetail.username + `</p>
+                </div>
+              </div>
+
+            <div class="row">
+              <div class="col-md-6">
+                <label>Nom et prénom:</label>
+              </div>
+              <div class="col-md-6">
+                <p id="Name&Firstname">` + userDetail.lastName + ` ` + userDetail.firstName +`</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-6">
+                <label>Email</label>
+              </div>
+              <div class="col-md-6">
+                <p id="email">` + userDetail.email + `</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-6">
+                <label>nombre d'achat</label>
+              </div>
+              <div class="col-md-6">
+                <p id="purchased_furniture_nbr">` + userDetail.purchasedFurnitureNbr + `</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-6">
+                <label>nombre de ventes</label>
+              </div>
+              <div class="col-md-6">
+                <p id="sold_furniture_nbr">` + userDetail.soldFurnitureNbr + `</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-6">
+                <label>status</label>
+              </div>
+              <div class="col-md-6">
+                <p id="waiting">` + userDetail.waiting + `</p>
+              </div>
+            </div>
+
+            <div class="col-md-2" style="display: flex"> 
+              <input type="submit" class="profile-edit-btn" name="btnAddMore"  id="approuver" value="approuver" style="color: #0062cc; margin:5px"/>
+              <input type="submit" class="profile-edit-btn" name="btnAddMore" id="refuser" value="refuser" style="color: red; margin:5px"/>
+            </div>
+          </div>         
+          <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            <div class="row">
+              <div class="col-md-6">
+                <label> Adresse </label>
+              </div>
+              <div class="col-md-6">
+                <p>` + userDetail.address.street + ` ` +  userDetail.address.buildingNumber + `,` +  userDetail.address.postcode + ` ` + userDetail.address.commune + ` ` + userDetail.address.country + `</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <label>Map</label><br/>
+                <div id="map" ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</form>           
+</div>
     `;
 }
 
 const clientDetail = async (id) => {
+  let userDetails;
   await fetch(`/users/detail/${id}`, {
     method: "GET",
     headers: {
@@ -243,11 +263,11 @@ const clientDetail = async (id) => {
     }
     return response.json();
   }).then((data) => {
-    userDetail = data;
+    userDetails = data;
   }).catch((err) => {
     console.error(err);
   });
-
+  return userDetails;
 }
 
 const map = (latitude, lngitude) => {
@@ -292,6 +312,36 @@ const map = (latitude, lngitude) => {
       })
     });
   }
+}
+
+const generateLoadingAnimation = () => {
+  return `
+      <div class="text-center">
+          <h2>Loading <div class="spinner-border"></div></h2>
+      </div>`
+}
+
+const getUserList = async () => {
+  let ret = [];
+  await fetch("/users/detail", {
+    method: "GET",
+    headers: {
+      "Authorization": currentUser.token,
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(
+          "Error code : " + response.status + " : " + response.statusText
+      );
+    }
+    return response.json();
+  }).then((data) => {
+    ret = data;
+  }).catch((err) => {
+    console.error(err);
+  });
+  return ret;
 }
 
 const AddressToGeo = async (address) => {
