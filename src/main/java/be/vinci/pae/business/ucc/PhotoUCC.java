95da7cf798190fad702733de1020856c0ac4f5d2
Lucas Agro <lucas.y.agro@student.vinci.be>
2021-03-30 19:25:06 +0200

    feat - create PhotoUCC and PhotoUCCImpl and implement method getAllHomePageVisiblePhotos in both classes

---
+++ src/main/java/be/vinci/pae/business/ucc/PhotoUCC.java
@@ -0,0 +1,16 @@
+package be.vinci.pae.business.ucc;
+
+import be.vinci.pae.business.dto.PhotoDTO;
+
+import java.util.List;
+
+public interface PhotoUCC {
+
+  /**
+   * get all the photos that are visible on the home page's carousel.
+   *
+   * @return a list containing all the photos that are visible on the home page's carousel.
+   */
+  List<PhotoDTO> getAllHomePageVisiblePhotos();
+
+}
---
+++ src/main/java/be/vinci/pae/business/ucc/PhotoUCCImpl.java
@@ -0,0 +1,39 @@
+package be.vinci.pae.business.ucc;
+
+import be.vinci.pae.business.dto.PhotoDTO;
+import be.vinci.pae.business.dto.UserDTO;
+import be.vinci.pae.persistence.dal.ConnectionDalServices;
+import be.vinci.pae.persistence.dao.AddressDAO;
+import be.vinci.pae.persistence.dao.PhotoDAO;
+import jakarta.inject.Inject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhotoUCCImpl implements PhotoUCC {
+
+  @Inject
+  private PhotoDAO photoDAO;
+
+  @Inject
+  private ConnectionDalServices dalServices;
+
+  /**
+   * get all the photos that are visible on the home page's carousel.
+   *
+   * @return a list containing all the photos that are visible on the home page's carousel.
+   */
+  @Override
+  public List<PhotoDTO> getAllHomePageVisiblePhotos() {
+    List<PhotoDTO> res = new ArrayList<>();
+    try {
+      dalServices.startTransaction();
+      res = photoDAO.getAllHomePageVisiblePhotos();
+      dalServices.commitTransaction();
+    } catch (Exception e) {
+      e.printStackTrace();
+      dalServices.rollbackTransaction();
+    }
+    return res;
+  }
+}
