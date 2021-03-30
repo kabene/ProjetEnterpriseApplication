1e984f31c832b331d2e97cfa8c6d1c6f3f9b3cb6
Lucas Agro <lucas.y.agro@student.vinci.be>
2021-03-30 19:52:33 +0200

    refactor - change ApplicationBinder and prod.properties

--- src/main/java/be/vinci/pae/main/ApplicationBinder.java
+++ src/main/java/be/vinci/pae/main/ApplicationBinder.java
@@ -4,6 +4,7 @@ package be.vinci.pae.main;
 import be.vinci.pae.business.factories.FurnitureFactory;
 import be.vinci.pae.business.factories.PhotoFactory;
 import be.vinci.pae.business.ucc.FurnitureUCC;
+import be.vinci.pae.business.ucc.PhotoUCC;
 import be.vinci.pae.persistence.dal.ConnectionBackendDalServices;
 import be.vinci.pae.persistence.dao.FurnitureDAO;
 import be.vinci.pae.persistence.dao.FurnitureTypeDAO;
@@ -26,34 +27,43 @@ public class ApplicationBinder extends AbstractBinder {
   @Override
   protected void configure() {
     try {
-      bind(Class.forName(Configurate.getConfiguration("UserFactory"))).to(UserFactory.class)
-          .in(Singleton.class);
-      bind(Class.forName(Configurate.getConfiguration("UserUCC"))).to(UserUCC.class)
-          .in(Singleton.class);
+      //DAO
       bind(Class.forName(Configurate.getConfiguration("UserDAO"))).to(UserDAO.class)
-          .in(Singleton.class);
-      bind(Class.forName(Configurate.getConfiguration("AddressFactory"))).to(AddressFactory.class)
           .in(Singleton.class);
       bind(Class.forName(Configurate.getConfiguration("AddressDAO"))).to(AddressDAO.class)
           .in(Singleton.class);
-      bind(Class.forName(Configurate.getConfiguration("ConnectionDalServices")))
-          .to(ConnectionDalServices.class).to(ConnectionBackendDalServices.class)
-          .in(Singleton.class);
-      bind(Class.forName(Configurate.getConfiguration("Authentication"))).to(Authentication.class)
-          .in(Singleton.class);
-      bind(Class.forName(Configurate.getConfiguration("FurnitureFactory")))
-          .to(FurnitureFactory.class).in(Singleton.class);
-      bind(Class.forName(Configurate.getConfiguration("FurnitureUCC"))).to(FurnitureUCC.class)
-          .in(Singleton.class);
       bind(Class.forName(Configurate.getConfiguration("FurnitureDAO"))).to(FurnitureDAO.class)
           .in(Singleton.class);
-      bind(Class.forName(Configurate.getConfiguration("PhotoFactory"))).to(PhotoFactory.class)
+      bind(Class.forName(Configurate.getConfiguration("FurnitureTypeDAO"))).to(FurnitureTypeDAO.class)
           .in(Singleton.class);
       bind(Class.forName(Configurate.getConfiguration("PhotoDAO"))).to(PhotoDAO.class)
           .in(Singleton.class);
-      bind(Class.forName(Configurate.getConfiguration("FurnitureTypeDAO")))
-          .to(FurnitureTypeDAO.class)
+
+      //FACTORY
+      bind(Class.forName(Configurate.getConfiguration("UserFactory"))).to(UserFactory.class)
           .in(Singleton.class);
+      bind(Class.forName(Configurate.getConfiguration("AddressFactory"))).to(AddressFactory.class)
+          .in(Singleton.class);
+      bind(Class.forName(Configurate.getConfiguration("FurnitureFactory"))).to(FurnitureFactory.class)
+          .in(Singleton.class);
+      bind(Class.forName(Configurate.getConfiguration("PhotoFactory"))).to(PhotoFactory.class)
+          .in(Singleton.class);
+
+      //UCC
+      bind(Class.forName(Configurate.getConfiguration("UserUCC"))).to(UserUCC.class)
+          .in(Singleton.class);
+      bind(Class.forName(Configurate.getConfiguration("FurnitureUCC"))).to(FurnitureUCC.class)
+          .in(Singleton.class);
+      bind(Class.forName(Configurate.getConfiguration("PhotoUCC"))).to(PhotoUCC.class)
+          .in(Singleton.class);
+
+      //OTHERS
+      bind(Class.forName(Configurate.getConfiguration("Authentication"))).to(Authentication.class)
+          .in(Singleton.class);
+      bind(Class.forName(Configurate.getConfiguration("ConnectionDalServices"))).to(ConnectionDalServices.class)
+          .to(ConnectionBackendDalServices.class)
+          .in(Singleton.class);
+
     } catch (ClassNotFoundException e) {
       e.printStackTrace();
     }
