8196bb9a0e1ac3b20eeb89bde70d104dc3013aa9
Lucas Agro <lucas.y.agro@student.vinci.be>
2021-03-30 19:00:27 +0200

    feat - add getAllHomePageVisibleMethod and create toDto method in PhotoDAOImpl by extracting duplicated code

--- src/main/java/be/vinci/pae/persistence/dao/PhotoDAO.java
+++ src/main/java/be/vinci/pae/persistence/dao/PhotoDAO.java
@@ -8,4 +8,11 @@ public interface PhotoDAO {
   List<PhotoDTO> getPhotosByFurnitureId(int furnitureId);
 
   PhotoDTO getPhotoById(int photoId);
+
+  /**
+   * Make a query to the database to get all the photos visible on the home page's carousel.
+   *
+   * @return a list containing all the photos visible on the home page's carousel.
+   */
+  List<PhotoDTO> getAllHomePageVisiblePhotos();
 }
