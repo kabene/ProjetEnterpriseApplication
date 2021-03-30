cde68031fc849d509c02cb7193ba94c67a52178d
Lucas Agro <lucas.y.agro@student.vinci.be>
2021-03-30 19:54:11 +0200

    feat - add PhotoResource and photos.http and add getVisibleHomePage on PhotoResource

---
+++ src/main/java/be/vinci/pae/presentation/PhotoResource.java
@@ -0,0 +1,45 @@
+package be.vinci.pae.presentation;
+
+import be.vinci.pae.business.dto.PhotoDTO;
+import be.vinci.pae.business.ucc.PhotoUCC;
+import be.vinci.pae.main.Main;
+import be.vinci.pae.presentation.filters.Admin;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Singleton
+@Path("/photos")
+public class PhotoResource {
+
+  @Inject
+  PhotoUCC photoUCC;
+
+  /**
+   * GET all photos that are visible in the home page's carousel
+   *
+   * @return http response containing a list full of all the visible photos in the home page's carousel in json format
+   */
+  @GET
+  @Path("/homePage")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getVisibleHomePage() {
+    Logger.getLogger(Main.CONSOLE_LOGGER_NAME).log(Level.INFO, "GET /photo/homePage");
+    List<PhotoDTO> photoDTOS = photoUCC.getAllHomePageVisiblePhotos();
+    List<PhotoDTO> res = new ArrayList<>();
+    for (PhotoDTO dto : photoDTOS) {
+      res.add(dto);
+    }
+    return Response.ok(res).build();
+  }
+
+}
