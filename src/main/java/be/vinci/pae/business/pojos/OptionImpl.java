7a5909a230f153641e710605602e3fd3c5ffbac7
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-04-01 16:47:01 +0200

    make user id public

--- src/main/java/be/vinci/pae/business/pojos/OptionImpl.java
+++ src/main/java/be/vinci/pae/business/pojos/OptionImpl.java
@@ -17,12 +17,12 @@ public class OptionImpl implements OptionDTO {
   @JsonView(Views.Public.class)
   private String dateOption;
   @JsonView(Views.Public.class)
-  private int clientId;
+  private int userId;
   @JsonView(Views.Public.class)
   private int furnitureId;
   @JsonView(Views.Public.class)
   private boolean isCanceled;
-  @JsonView(Views.AdminOnly.class)
+  @JsonView(Views.Public.class)
   private UserDTO user;
 
   @Override
@@ -57,12 +57,12 @@ public class OptionImpl implements OptionDTO {
 
   @Override
   public int getUserId() {
-    return clientId;
+    return userId;
   }
 
   @Override
   public void setUserId(int clientId) {
-    this.clientId = clientId;
+    this.userId = clientId;
   }
 
   @Override
