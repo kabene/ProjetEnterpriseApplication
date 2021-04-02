bb910795f8298efb1313c7f523575099ba8d07a6
Thibaut Gérard <thibaut.t.gerard@student.vinci.be>
2021-04-02 17:08:58 +0200

    refactor - adress footer

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     let footerHtml = `   
         <div class="footer text-center justify-content-center mt-2 mb-2">
             <img id="logoFooter" src="` + logo + `" alt="logo" />
-            <span>1bis sentier des artistes – Verviers, num, codeCommune</span>
+            <span>1bis sentier des artistes – 4800, Verviers, Belgique</span>
         </div>  
     `;
     footer.innerHTML = footerHtml;
