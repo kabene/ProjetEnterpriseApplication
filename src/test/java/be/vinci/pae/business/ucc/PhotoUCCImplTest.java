54be99d9552cefe339868ac3ec41224691b746d9
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-04-22 20:38:39 +0200

    feat - test new ucc methods

--- src/test/java/be/vinci/pae/business/ucc/PhotoUCCImplTest.java
+++ src/test/java/be/vinci/pae/business/ucc/PhotoUCCImplTest.java
@@ -2,6 +2,7 @@ package be.vinci.pae.business.ucc;
 
 import be.vinci.pae.business.dto.PhotoDTO;
 import be.vinci.pae.business.pojos.PhotoImpl;
+import be.vinci.pae.exceptions.NotFoundException;
 import be.vinci.pae.main.TestBinder;
 import be.vinci.pae.persistence.dal.ConnectionDalServices;
 import be.vinci.pae.persistence.dao.PhotoDAO;
@@ -11,6 +12,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InOrder;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
@@ -30,6 +34,10 @@ class PhotoUCCImplTest {
   private static PhotoDTO mockPhotoDTO1;
   private static PhotoDTO mockPhotoDTO2;
   private static PhotoDTO mockPhotoDTO3;
+
+  private static int defaultPhotoId1 = 1;
+  private static int defaultPhotoId2 = 2;
+  private static int defaultPhotoId3 = 3;
 
   @BeforeAll
   public static void init() {
@@ -51,6 +59,14 @@ class PhotoUCCImplTest {
     Mockito.reset(mockPhotoDTO1);
     Mockito.reset(mockPhotoDTO2);
     Mockito.reset(mockPhotoDTO3);
+
+    Mockito.when(mockPhotoDAO.getPhotoById(defaultPhotoId1)).thenReturn(mockPhotoDTO1);
+    Mockito.when(mockPhotoDAO.getPhotoById(defaultPhotoId2)).thenReturn(mockPhotoDTO2);
+    Mockito.when(mockPhotoDAO.getPhotoById(defaultPhotoId3)).thenReturn(mockPhotoDTO3);
+
+    Mockito.when(mockPhotoDTO1.getPhotoId()).thenReturn(defaultPhotoId1);
+    Mockito.when(mockPhotoDTO2.getPhotoId()).thenReturn(defaultPhotoId2);
+    Mockito.when(mockPhotoDTO3.getPhotoId()).thenReturn(defaultPhotoId3);
   }
 
 
@@ -109,4 +125,57 @@ class PhotoUCCImplTest {
     Mockito.verify(mockDal, Mockito.never()).commitTransaction();
   }
 
+  @DisplayName("TEST PhotoUCC.patchVisibility() : nominal scenario")
+  @ParameterizedTest
+  @ValueSource(booleans = {true, false})
+  void test_patchVisibility_nominalScenario_shouldReturnDTO(boolean visibility) {
+    Mockito.when(mockPhotoDAO.updateIsVisible(mockPhotoDTO1)).thenReturn(mockPhotoDTO2);
+
+    PhotoDTO actual = photoUCC.patchVisibility(defaultPhotoId1, visibility);
+    assertEquals(mockPhotoDTO2, actual);
+
+    InOrder inOrder = Mockito
+        .inOrder(mockPhotoDTO1, mockPhotoDAO, mockDal); // enforce invocation order
+
+    inOrder.verify(mockDal).startTransaction();
+    inOrder.verify(mockPhotoDTO1).setVisible(visibility);
+    inOrder.verify(mockPhotoDAO).updateIsVisible(mockPhotoDTO1);
+    inOrder.verify(mockDal).commitTransaction();
+
+    Mockito.verify(mockDal, Mockito.never()).rollbackTransaction();
+  }
+
+  @DisplayName("TEST PhotoUCC.patchVisibility() : invalid id should throw NotFoundException after rollback")
+  @ParameterizedTest
+  @ValueSource(booleans = {true, false})
+  void test_patchVisibility_givenInvalidId_shouldThrowNotFound(boolean visibility) {
+    Mockito.when(mockPhotoDAO.updateIsVisible(mockPhotoDTO1)).thenThrow(new NotFoundException());
+
+    assertThrows(NotFoundException.class, () -> {
+      photoUCC.patchVisibility(defaultPhotoId1, visibility);
+    });
+
+    InOrder inOrder = Mockito.inOrder(mockDal);
+
+    inOrder.verify(mockDal).startTransaction();
+    inOrder.verify(mockDal).rollbackTransaction();
+    Mockito.verify(mockDal, Mockito.never()).commitTransaction();
+  }
+
+  @DisplayName("TEST PhotoUCC.patchVisibility() : invalid id should throw NotFoundException after rollback")
+  @ParameterizedTest
+  @ValueSource(booleans = {true, false})
+  void test_patchVisibility_catchesInternalError_shouldThrowInternal(boolean visibility) {
+    Mockito.when(mockPhotoDAO.updateIsVisible(mockPhotoDTO1)).thenThrow(new InternalError());
+
+    assertThrows(InternalError.class, () -> {
+      photoUCC.patchVisibility(defaultPhotoId1, visibility);
+    });
+
+    InOrder inOrder = Mockito.inOrder(mockDal);
+
+    inOrder.verify(mockDal).startTransaction();
+    inOrder.verify(mockDal).rollbackTransaction();
+    Mockito.verify(mockDal, Mockito.never()).commitTransaction();
+  }
 }
