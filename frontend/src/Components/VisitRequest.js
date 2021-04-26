a0fa1afc89dadcbbaa9b47af8e290b746d937657
Karim Abene <karim.abene@student.vinci.be>
2021-04-26 15:28:02 +0200

    feature - begin

--- frontend/src/Components/VisitRequest.js
+++ frontend/src/Components/VisitRequest.js
@@ -1,8 +1,240 @@
-let page = document.querySelector("#page");
+import {getUserSessionData} from "../utils/session";
+import {displayErrorMessage, generateLoadingAnimation} from "../utils/utils";
+import {
+  displayErrorMessage,
+  importAllFurnitureImg,
+  findFurnitureImgSrcFromFilename,
+  findFavImgSrc,
+  generateLoadingAnimation
+} from "../utils/utils.js"
 
-const VisitRequest = () => {
-    let pageHTML = `<h1>Page pour demander une visite du site (clients)</h1>`;;
-    page.innerHTML = pageHTML;
+let page = document.querySelector("#page");
+let currentUser;
+let mainPage;
+let requestList;
+let requestMap = [];
+let timeouts = [];
+let currentUser;
+let pageHTML;
+let openTab = "infos";
+
+const VisitRequest = async (id) => {
+  currentUser = getUserSessionData();
+  pageHTML = `
+  <div class="col-5 mx-auto"><div id="errorDiv" class="d-none"></div></div>
+  <div id="mainPage" class="col-12 px-0">${generateLoadingAnimation()}</div>`;
+  page.innerHTML = pageHTML;
+  mainPage = document.querySelector("#mainPage");
+  await findVisitRequestList();
+  if (!id) {
+    pageHTML = generatePageHtml();
+
+    mainPage.innerHTML = pageHTML;
+
+    document.querySelectorAll(".toBeClicked").forEach(
+        element => element.addEventListener("click", displayShortElements));
+    document.querySelector("#buttonReturn").addEventListener("click",
+        displayLargeTable);
+  } else {
+    loadCard(id);
+  }
+
+}
+//displayers
+
+
+
+const displayLargeTable = () => {
+  openTab = "infos";
+  document.querySelector('#shortTableContainer').id = "largeTableContainer";
+  timeouts.push(setTimeout(displayLargeElements, 750));
+  document.querySelectorAll(".shortElement").forEach(
+      element => element.className = "shortElement d-none");
+  document.querySelector('#shortTable').id = "largeTable";
+  document.querySelectorAll(".toBeClicked").forEach(
+      element => element.className = "toBeClicked");
+  document.querySelector("#buttonReturn").className = "btn btn-dark m-3 d-none";
+  document.querySelectorAll("#thumbnail").forEach(
+      element => element.className = "w-50 mx-auto");
+  document.querySelectorAll(".tableStatus").forEach(element => {
+    let status = element.getAttribute("status");
+    let infos = generateStatusInfos(status);
+    element.innerHTML = `<p class="text-${infos.classname}">${infos.status}</p>`
+  })
+}
+
+const displayLargeElements = () => {
+  document.querySelectorAll('.notNeeded').forEach(
+      element => element.className = "notNeeded align-middle");
+}
+const displayShortElements = async (e) => {
+  removeTimeouts();
+  //hide large table
+  let largeTable = document.querySelector('#largeTable');
+  if (largeTable !== null) {
+    largeTable.id = "shortTable";
+  }
+  if (document.querySelector('#largeTableContainer') !== null) {
+    timeouts.push(setTimeout(changeContainerId, 1000));
+  }
+  document.querySelectorAll(".notNeeded").forEach(
+      element => element.className = "notNeeded d-none");
+  document.querySelectorAll("#thumbnail").forEach(
+      element => element.className = "w-100 mx-auto");
+  document.querySelectorAll(".shortElement").forEach(
+      element => element.className = "shortElement");
+  let returnBtn = document.querySelector("#buttonReturn");
+  returnBtn.className = "btn btn-dark m-3";
+  let requestCardDiv = document.querySelector("#RequestCardDiv");
+  requestCardDiv.innerHTML = generateLoadingAnimation();
+
+  document.querySelectorAll(".toBeClicked").forEach(element => {
+    element.className = "toBeClicked";
+  });
+
+  let element = e.srcElement;
+  while (!element.className.includes("toBeClicked")) {
+    element = element.parentElement;
+  }
+  element.className = "toBeClicked bg-secondary text-light";
+
+  document.querySelectorAll(".tableStatus").forEach(element => {
+    let status = element.attributes["status"].value;
+    let classname = generateStatusInfos(status).classname;
+    element.innerHTML = generateDot(classname);
+  });
+  let id = element.attributes["requestId"].value;
+  let request = requestMap[id];
+  if (!request) {
+    await reloadPage();
+  }
+
+}
+
+// generators
+
+/**
+ * Reloads the page and re-fetch furniture information.
+ * Displays loading animation while awaiting the fetch.
+ */
+const reloadPage = async () => {
+  mainPage.innerHTML = generateLoadingAnimation();
+  await findVisitRequestList();
+  mainPage = generatePageHtml();
+}
+
+
+const generatePageHtml = (largeTable = true) => {
+  let tableSize = "large";
+  let notNeededClassName = "notNeeded align-middle";
+  let shortElementClassName = "shortElement d-none";
+  if (largeTable === false) {
+    tableSize = "short";
+    notNeededClassName = "notNeeded d-none";
+    shortElementClassName = "shortElement";
+  }
+  let res = `
+  <div id="${tableSize}TableContainer" class="px-0">
+    <div>
+      <button type="button" id="buttonReturn" class="btn btn-dark m-3 ${shortElementClassName}">Retour à la liste</button>
+      <table id="${tableSize}Table" class="table table-hover border border-1 text-center">
+        <thead class="table-secondary">
+          <tr class="">
+            <th class="w-25"></th>
+            <th class="align-middle">Description</th>
+            <th class="${notNeededClassName}">Client</th>
+            <th class="align-middle">État</th>
+            <th class="${notNeededClassName}">Adresse</th>
+            <th class="${notNeededClassName}">Date de la demande</th>
+            <th class="${notNeededClassName}">Etat</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${generateAllRows(notNeededClassName)}
+        </tbody>
+      </table>
+    </div>
+    <div class="shortElement ${shortElementClassName}" id="RequestCardDiv">Hello</div>
+  </div>
+    
+  </div>`;
+  return res;
+}
+
+
+const generateDot = (colorClassName) => {
+  return `<span class="badge badge-pill p-1 badge-${colorClassName}"> </span>`;
+}
+
+const generateStatusInfos = (status) => {
+  let res = {
+    classname: "",
+    status: "",
+  }
+
+  switch (status) {
+    case "confimed":
+      res.classname = "success";
+      res.status = "Accepté";
+      break;
+    case "canceled":
+      res.classname = "dark";
+      res.status = "Refusé";
+      break;
+    default:
+      res.classname = "";
+      res.status = status;
+  }
+  return res;
+}
+
+
+
+
+const removeTimeouts = () => {
+  timeouts.forEach(timeout => {
+    clearTimeout(timeout);
+  })
+}
+
+const changeContainerId = () => {
+  document.querySelector('#largeTableContainer').id = "shortTableContainer";
+}
+
+
+
+
+
+
+//requests
+
+async function findVisitRequestList() {
+  return fetch("/requestForVisit/", {
+    method: "GET",
+    headers: {
+      "Authorization": currentUser.token,
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(
+          response.status + " : " + response.statusText
+      );
+    }
+    return response.json();
+  }).then((data) => {
+    requestList = data;
+    requestList.forEach(request => {
+      requestMap[request.requestId] = request;
+    });
+  }).catch((err) => {
+    console.log("Erreur de fetch !! :´\n" + err);
+    displayErrorMessage("errorDiv", err);
+  });
+}
+
+const displayUsercard = async () => {
+
 }
 
 export default VisitRequest;
