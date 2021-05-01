c63cfc3df83bb9b0e42bf1d11e5c9f9bccaf8bb4
Lucas Agro <lucas.y.agro@student.vinci.be>
2021-05-01 15:53:28 +0200

    feat - add methods for filtering carousel

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -1,30 +1,96 @@
 import notFoundPhoto from "../img/notFoundPhoto.png";
-import {displayErrorMessage, importAllFurnitureImg, findFurnitureImgSrcFromFilename, generateLoadingAnimation} from "../utils/utils.js";
+import {displayErrorMessage, generateLoadingAnimation} from "../utils/utils.js";
 
 let page = document.querySelector("#page");
+
 let visiblePhotos;
-let images = importAllFurnitureImg();
+let furnitureTypeList;
+
+let filterType = "";
 
 const HomePage = async () => {
 	page.innerHTML = generateLoadingAnimation();
-	visiblePhotos;
-	await getVisiblePhotos();
-	console.log(visiblePhotos);
+	visiblePhotos = await getVisiblePhotos();
+	furnitureTypeList = await getFurnitureTypeList();
 
 	page.innerHTML = getPageHTML();
+
+	addAllEventListeners();
 }
+
+
+/********************  Business methods  **********************/
+
+
+/**
+ * Called when clicking on the apply filter button.
+ * Apply all filters chosen on the furniture and render them.
+ */
+ const onClickApplyFilter = (e) => {
+  e.preventDefault();
+  filterType = document.querySelector("#furnitureTypeFilter").value;
+	page.innerHTML = getPageHTML();
+  addAllEventListeners();
+  document.querySelector("[value='" + filter.type + "']").setAttribute('selected', 'true');
+}
+
+/**
+ * Called when clicking on the cancel filter button.
+ * Clear all filters chosen on the furniture and render all the furniture if there already was filters.
+ */
+const onClickClearFilter = (e) => {
+  e.preventDefault();
+  if (filterType === "")
+    return;
+  filterType = "";
+  page.innerHTML = getPageHTML();
+  addAllEventListeners();
+}
+
+/**
+ * Add all the event listeners required in the document.
+ */
+ const addAllEventListeners = () => {
+  document.querySelector("#apply-filters-btn").addEventListener("click", onClickApplyFilter);
+  document.querySelector("#clear-filters-btn").addEventListener("click", onClickClearFilter);
+}
+
+/********************  HTML generation  **********************/
 
 const getPageHTML = () => {
 	return `
 	<div class="col-5 mx-auto">
-        <div id="errorDiv" class="d-none"></div>
-    </div>
-    <div class="row mx-0 pt-5">
-        <div class="col-2 col-lg-4"></div>
-        <div class="col-8 col-lg-4">` + getCarousel() + `</div>
-        <div class="col-2 col-lg-4"></div>
-    </div>
-    `;
+    <div id="errorDiv" class="d-none"></div>
+  </div>
+	<div>
+    <h3>Filtrer les meubles:</h3>
+    ` + generateFilterHTML() + `
+  </div>
+  <div class="row mx-0 pt-5">
+    <div class="col-2 col-lg-4"></div>
+    <div class="col-8 col-lg-4">` + getCarousel() + `</div>
+    <div class="col-2 col-lg-4"></div>
+  </div>`;
+}
+
+const generateFilterHTML = () => {
+  return `
+  <form class="form-inline">
+    <div class="form-group m-3">` + generateSelectTypeTag() + `</div>
+     <button type="submit" id="apply-filters-btn" class="btn btn-primary m-3">Appliquer</button>
+     <button type="submit" id="clear-filters-btn" class="btn btn-secondary m-3">Retirer les filtres</button>
+  </form>`;
+}
+
+const generateSelectTypeTag = () => {
+  let ret = `<select class="form-control" id="furnitureTypeFilter"> <option value="">Rechercher un type de meuble</option>`;
+  furnitureTypeList.forEach(type => ret += generateOptionTypeTag(type));
+  ret += `</select>`;
+  return ret;
+}
+
+const generateOptionTypeTag = (type) => {
+  return `<option value="` + type.typeName + `">` + type.typeName + `</option>`;
 }
 
 const getCarousel = () => {
@@ -74,23 +140,34 @@ const getHTMLVisiblePhotos = () => {
 	return ret;
 }
 
+
+/********************  Backend fetch  **********************/
+
 const getVisiblePhotos = async () => {
-	await fetch("/photos/homePage", {
+	let response = await fetch("/photos/homePage", {
 		method: "GET",
 		headers: {
 			"Content-Type": "application/json",
 		},
-	}).then((response) => {
-		if (!response.ok) {
-			throw new Error("Error code : " + response.status + " : " + response.statusText);
-		}
-		return response.json();
-	}).then((data) => {
-		visiblePhotos = data;
-	}).catch((err) => {
-        console.log("Erreur de fetch !! :´<\n" + err);
-        displayErrorMessage("errorDiv", err);
-      });
+	});
+	if (!response.ok) {
+    const err = "Erreur de fetch\nError code : " + response.status + " : " + response.statusText;
+    console.error(err);
+    displayErrorMessage("errorDiv", err);
+  }
+	return response.json();
 }
+
+const getFurnitureTypeList = async () => {
+	let response = await fetch("/furnitureTypes/", {
+	  method: "GET",
+	});
+	if (!response.ok) {
+	  const err = "Erreur de fetch\nError code : " + response.status + " : " + response.statusText;
+	  console.error(err);
+	  displayErrorMessage("errorDiv", err);
+	}
+	return response.json();
+  }
 
 export default HomePage;
