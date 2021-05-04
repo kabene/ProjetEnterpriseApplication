7b916e2111b452208ab76165081b11bcdb42ccdf
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-05-04 11:54:59 +0200

    feat - implement lazy loading in homepage

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -17,8 +17,8 @@ let nbrPhotosInCarousel;
 
 const HomePage = async () => {
 	page.innerHTML = errorDiv + generateLoadingAnimation();
-	visiblePhotos = await getVisiblePhotos();
 	furnitureTypeList = await getFurnitureTypeList();
+	getVisiblePhotos();
 	console.log(visiblePhotos);
 
 	page.innerHTML = errorDiv + getPageHTML();
@@ -102,22 +102,28 @@ const generateOptionTypeTag = (type) => {
 }
 
 const getCarousel = () => {
-	let photos = getHTMLVisiblePhotos();
-	return `<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-						<ol class="carousel-indicators bg-secondary"> ` + getHTMLCarouselIndicators() + ` </ol>
-						<div class="carousel-inner">
-							` + photos + `
-						</div>
-						<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-							<span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
-							<span class="sr-only">Previous</span>
-						</a>
-						<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-							<span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
-							<span class="sr-only">Next</span>
-						</a>
-					</div>
-`;
+	if(visiblePhotos){
+		let photos = getHTMLVisiblePhotos();
+		return `
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators bg-secondary"> ` + getHTMLCarouselIndicators() + ` </ol>
+			<div class="carousel-inner">
+				` + photos + `
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+		`;
+	} else {
+		return generateLoadingAnimation();
+	}
+	
 }
 
 const getHTMLCarouselIndicators = () => {
@@ -160,10 +166,15 @@ const getHTMLVisiblePhotos = () => {
 	return ret;
 }
 
+const getVisiblePhotos = async () => {
+	let data = await fetchVisiblePhotos();
+	visiblePhotos = data;
+	page.innerHTML = getPageHTML();
+}
 
 /********************  Backend fetch  **********************/
 
-const getVisiblePhotos = async () => {
+const fetchVisiblePhotos = async () => {
 	let response = await fetch("/photos/homePage", {
 		method: "GET",
 		headers: {
