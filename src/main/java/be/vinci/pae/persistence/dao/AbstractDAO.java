21237f51aa24a99d769230a7bd705f2b4f87963b
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-05-10 14:43:36 +0200

    fix - pmd

--- src/main/java/be/vinci/pae/persistence/dao/AbstractDAO.java
+++ src/main/java/be/vinci/pae/persistence/dao/AbstractDAO.java
@@ -9,9 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public abstract class AbstractDAO {
 
