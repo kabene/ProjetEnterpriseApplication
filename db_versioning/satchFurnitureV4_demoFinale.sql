50f636f61f141c2f46402c5b480ae2bd40e3dea7
kabene <17karimabene@gmail.com>
2021-05-11 15:28:36 +0200

    fix - change addresses

--- db_versioning/satchFurnitureV4_demoFinale.sql
+++ db_versioning/satchFurnitureV4_demoFinale.sql
@@ -168,13 +168,13 @@ VALUES (DEFAULT, 'sente des artistes', '1bis', NULL , '4800', 'Vervier', 'Belgiq
 INSERT
 INTO satchoFurniture.users (user_id, last_name, first_name, username, email, address_id, registration_date, role,
                             password, purchased_furniture_nbr, sold_furniture_nbr, is_waiting)
-VALUES (DEFAULT, 'Satcho', 'Albert', 'bert', 'bert.satcho@gmail.be', 1, now(), 'admin',
+VALUES (DEFAULT, 'Satcho', 'Albert', 'bert', 'bert.satcho@gmail.be', 2, now(), 'admin',
         '$2a$04$WWrY8UJlV4bysHDwXVD3.uclmB5AT7oSb78bDd4/6Iq7aHqUrfVhi', 0, 0, 'false');
 
 INSERT INTO satchoFurniture.addresses
 VALUES (DEFAULT, 'sente des artistes', '18', NULL , '4800', 'Vervier', 'Belgique');
 INSERT INTO satchoFurniture.users
-VALUES (DEFAULT, 'Satcho', 'Laurent', 'lau', 'laurent.satcho@gmail.be', 2, now(), 'admin',
+VALUES (DEFAULT, 'Satcho', 'Laurent', 'lau', 'laurent.satcho@gmail.be', 3, now(), 'admin',
         '$2a$04$nppXs4TmjaX/NmV0PYdku.7HW4X9VM1K4KImzrCF8LnGYlQOJSQnu', 0, 0, 'false');
 
 
@@ -182,29 +182,29 @@ VALUES (DEFAULT, 'Satcho', 'Laurent', 'lau', 'laurent.satcho@gmail.be', 2, now()
 INSERT INTO satchoFurniture.addresses
 VALUES (DEFAULT, 'Rue de l’Eglise', '11', 'B1' , '4987', 'Stoumont', 'Belgique');
 INSERT INTO satchoFurniture.users
-VALUES (DEFAULT, 'Line', 'Caroline', 'Caro', 'caro.line@hotmail.com', 3, now(), 'antique_dealer',
+VALUES (DEFAULT, 'Line', 'Caroline', 'Caro', 'caro.line@hotmail.com', 4, now(), 'antique_dealer',
         '$2a$04$rvF3zWgvRyeKz2qSBeud5uXfAcHDkMeJWsOez01YPomtSBBqPTmmO', 0, 5, 'false');
 
 INSERT INTO satchoFurniture.addresses
 VALUES (DEFAULT, 'Rue de Renkin', '7', NULL , '4800', 'Vervier', 'Belgique');
 INSERT INTO satchoFurniture.users
-VALUES (DEFAULT, 'Ile', 'Achille', 'achil', 'ach.ile@gmail.com', 4, now(), 'customer',
+VALUES (DEFAULT, 'Ile', 'Achille', 'achil', 'ach.ile@gmail.com', 5, now(), 'customer',
         '$2a$04$rvF3zWgvRyeKz2qSBeud5uXfAcHDkMeJWsOez01YPomtSBBqPTmmO', 0, 2, 'false');
 
 INSERT INTO satchoFurniture.addresses
 VALUES (DEFAULT, 'Lammerskreuzstrasse', '6', NULL , '52159', 'Roetgen', 'Allemagne');
 INSERT INTO satchoFurniture.users
-VALUES (DEFAULT, 'Ile', 'Basile', 'bazz', 'bas.ile@gmail.be', 5, now(), 'customer',
+VALUES (DEFAULT, 'Ile', 'Basile', 'bazz', 'bas.ile@gmail.be', 6, now(), 'customer',
         '$2a$04$rvF3zWgvRyeKz2qSBeud5uXfAcHDkMeJWsOez01YPomtSBBqPTmmO', 0, 3, 'false');
 
 INSERT INTO satchoFurniture.users
-VALUES (DEFAULT, 'Ile', 'Théophile', 'Theo', 'theo.phile@proximus.be', 4 , now(), 'antique_dealer',
+VALUES (DEFAULT, 'Ile', 'Théophile', 'Theo', 'theo.phile@proximus.be', 5 , now(), 'antique_dealer',
         '$2a$04$UHAY4jc17zGro8tm3aNd4u90ap/N4/ZIAl07qHCT1/Yd.RocMzvum', 0, 0, 'false');
 
 INSERT INTO satchoFurniture.addresses
 VALUES (DEFAULT, 'Rue des Minières', '45', 'Ter' , '4800', 'Verviers', 'Belgique');
 INSERT INTO satchoFurniture.users
-VALUES (DEFAULT, 'Line', 'Charles', 'Charline', 'charline@proximus.be', 6 , now(), 'customer',
+VALUES (DEFAULT, 'Line', 'Charles', 'Charline', 'charline@proximus.be', 7 , now(), 'customer',
         '$2a$04$Jiu02H9pbvg6a83RNVVBXezBnxI2qtPRYzm4ZANRtZXywH0guqo9.', 0, 0, 'false');
 
 
@@ -225,7 +225,7 @@ VALUES (DEFAULT, '25-03-21', 'tous les jours de 15h à 18h', 6, '29-03-21 15:00:
 
 
 INSERT INTO satchoFurniture.addresses
-VALUES (DEFAULT, 'Rue Victor Buillenne', '9', '4C' , '4800', 'Verviers', 'Belgique');
+VALUES (DEFAULT, 'Rue Victor Bouillenne', '9', '4C' , '4800', 'Verviers', 'Belgique');
 
 INSERT
 INTO satchoFurniture.requests_for_visit (request_id, request_date, time_slot, address_id, visit_date_time, explanatory_note, status, user_id)
